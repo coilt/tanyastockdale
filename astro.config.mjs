6fe156e4976982eab7d967a80b212a2dd46aa7fd
coilt <cameraboi@gmail.com>
2024-12-11 21:31:00 +0700

    Add GSAP animation

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from '@astrojs/tailwind';
  
+
+ 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
-});
+  integrations: [
+    tailwind(), 
+    vue(),]}
+
+
+);
 
 
 
--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,11 +1,12 @@
+
+
 <template>
   <div class="bg-white py-16 sm:py-24 lg:py-32 rounded-t-xxl">
     <div
       class="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8"
     >
       <h2
-        class="max-w-xl text-3xl font-semibold tracking-tight text-balance text-avocado sm:text-4xl lg:col-span-7"
-      >
+        class="max-w-xl text-3xl font-semibold tracking-tight text-balance text-avocado sm:text-4xl lg:col-span-7">
         Get the new articles first. Sign up for my newsletter.
       </h2>
       <form class="w-full max-w-md lg:col-span-5 lg:pt-2">
