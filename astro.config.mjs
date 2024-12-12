db0ab126e1f7331b82339aa39f6e2bd2fd88e141
coilt <cameraboi@gmail.com>
2024-12-12 18:10:52 +0700

    Clean up hero text size on different media

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
 import tailwind from '@astrojs/tailwind';
- 
 
- 
+
+
+import tunnel from 'astro-tunnel';
+
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    vue(),]}
+  integrations: [tailwind(), vue(), tunnel()]}
 
 
 );
-
-
-
- 
- 
-
- 
