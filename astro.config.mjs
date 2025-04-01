c92e7f2d2d89dcc08adfac69df9f58a1757c7505
coilt <cameraboi@gmail.com>
2025-04-01 09:22:38 +0700

    Add nodejs adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from '@astrojs/node';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import tunnel from "astro-tunnel";
@@ -8,6 +9,9 @@ import react from "@astrojs/react";
 
 export default defineConfig({
   output: 'server',
+  adapter: node({
+    mode: 'standalone' // This is important for Coolify deployments
+  }),
   srcDir: "./src",
   integrations: [
     tailwind(),
